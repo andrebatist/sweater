46f67ab66193616d95ae70006709ba8f4cab44ab
aplaksin <aplaksin@ecocompany.ru>
2020-02-05 17:00:20 +0300

    bean validation, шифрование паролей

--- src/main/resources/db/migration/V2__Add_admin.sql
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -1,5 +1,5 @@
 insert into usr (id, username, password, active)
-    value (1, 'admin', '123', true);
+    value (1, 'admin', '$2y$12$tYpfqtbE0AmdPK.GxYZyi.6tUM.zoLoYZ/UwOpA0.WHyb97PCvpvK', true);
 
 insert into user_role (user_id, roles)
     values (1, 'USER'),(1,'ADMIN');
